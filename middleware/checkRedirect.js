fa8ab6a9004fdd5d7478428c62bf31468b33a3e5
JordanMicle2019 <JordanMicle2019@gmail.com>
2021-12-01 18:47:22 +0800

    update header

--- middleware/checkRedirect.js
+++ middleware/checkRedirect.js
@@ -5,7 +5,7 @@ const {
 module.exports = (req, res, next) => {
     if (req.originalUrl.includes(".mp4")) {
         res.redirect(303, "https://pmd.broadcastcloud.tv" + req.originalUrl);
-        return saveUserInfo(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
+        return saveUserInfo(req.headers['X-Real-IP'] || req.connection.remoteAddress);
     }
 
     next();
